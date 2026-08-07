a16c0fa3f87f07c2bb69eb51929eda86fe1ace56
Coding Wars <ind910378369@hotmail.com>
2026-08-07 12:14:37 -0700

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Simple Hello World! program.
+        System.out.println("Hello World!"); // This will print "Hello World!"
+    }
+}
