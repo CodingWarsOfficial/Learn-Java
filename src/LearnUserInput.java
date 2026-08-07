54c60830c33d65173fc11fbacb67dc6bf9a7643d
Coding Wars <ind910378369@hotmail.com>
2026-08-07 12:22:53 -0700

    Initial Commit

---
+++ src/LearnUserInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LearnUserInput {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in); // Scanner Object
+        System.out.println("Enter your marks.");
+        int marks = obj.nextInt();
+        if (marks >= 60){
+            System.out.println("First Division.");
+        } else if (marks >= 50) {
+            System.out.println("Second Division.");
+        } else if (marks >= 33) {
+            System.out.println("Third Division.");
+        }else {
+            System.out.println("Fail.");
+        }
+        obj.close();
+    }
+}
