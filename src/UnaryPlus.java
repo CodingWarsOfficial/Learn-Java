40044b3cda0097e0ede66cdb142c04983604775d
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:51:24 -0700

    Initial Commit

---
+++ src/UnaryPlus.java
@@ -0,0 +1,18 @@
+/**
+ * Demonstrates the use of Unary Plus (+) operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class UnaryPlus {
+    public static void main(String[] args) {
+
+        // Store a number
+        int number = 10;
+
+        // Apply the unary plus operator
+        int result = +number;
+
+        // Display the result
+        System.out.println("Unary Plus: " + result);
+    }
+}
