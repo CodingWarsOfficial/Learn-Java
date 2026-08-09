bc28598a72375fc61aeb3962942247aba50615dd
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:47:10 -0700

    Initial Commit

---
+++ src/ArithmeticSubtraction.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Subtraction (-) arithmetic operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class ArithmeticSubtraction {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 10;
+
+        // Subtract b from a using the subtraction operator
+        int result = a - b;
+
+        // Display the result
+        System.out.println("Subtraction: " + result);
+    }
+}
