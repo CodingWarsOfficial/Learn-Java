ec4aeea740dc0eeefb38fafddfdc90c7afed61e8
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:43:42 -0700

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Demonstrates the basic Hello World program in Java.
+ *
+ * @author Coding Wars
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        // Display "Hello World!" on the console
+        System.out.println("Hello World!");
+    }
+}
