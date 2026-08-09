8a280f7de06c167bcd8c477d5bfd6ff4d4af5030
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:48:06 -0700

    Initial Commit

---
+++ src/ArithmeticMultiplication.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Multiplication (*) arithmetic operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class ArithmeticMultiplication {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 10;
+
+        // Multiply both numbers using the multiplication operator
+        int result = a * b;
+
+        // Display the result
+        System.out.println("Multiplication: " + result);
+    }
+}
