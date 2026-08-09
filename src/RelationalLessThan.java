bc095956c826a1e76dab1b2ef29ef1a9faeeded4
Coding Wars <ind910378369@hotmail.com>
2026-08-09 07:21:34 -0700

    Initial Commit

---
+++ src/RelationalLessThan.java
@@ -0,0 +1,20 @@
+/**
+ * Demonstrates the use of Less Than (<) relational operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class RelationalLessThan {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 10;
+        int b = 20;
+
+        // Check whether a is less than b
+        if (a < b) {
+            System.out.println(a + " is less than " + b);
+        } else {
+            System.out.println(a + " is not less than " + b);
+        }
+    }
+}
