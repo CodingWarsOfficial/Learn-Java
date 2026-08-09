f3c8bcbbdcb7f66dd548ee990ceaa05b12250b40
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:40:19 -0700

    Initial Commit

---
+++ src/RelationalNotEqualTo.java
@@ -0,0 +1,20 @@
+/**
+ * Demonstrates the use of Not Equal To (!=) relational operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class RelationalNotEqualTo {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 10;
+        int b = 20;
+
+        // Check whether both numbers are different
+        if (a != b) {
+            System.out.println("Both numbers are different.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+    }
+}
