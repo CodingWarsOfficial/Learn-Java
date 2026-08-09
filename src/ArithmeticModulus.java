c2a857680087561c0480067b0bd9e07c69f49899
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:49:45 -0700

    Initial Commit

---
+++ src/ArithmeticModulus.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Modulus (%) arithmetic operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class ArithmeticModulus {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 6;
+
+        // Find the remainder using the modulus operator
+        int result = a % b;
+
+        // Display the remainder
+        System.out.println("Modulus: " + result);
+    }
+}
