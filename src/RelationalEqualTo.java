143b5c959a571f06ce3db27dae1fa77c6c85d8e5
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:38:59 -0700

    Initial Commit

---
+++ src/RelationalEqualTo.java
@@ -0,0 +1,20 @@
+/**
+ * Demonstrates the use of Equal To (==) relational operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class RelationalEqualTo {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 10;
+        int b = 10;
+
+        // Check whether both numbers are equal
+        if (a == b) {
+            System.out.println("Both numbers are equal.");
+        } else {
+            System.out.println("Both numbers are not equal.");
+        }
+    }
+}
