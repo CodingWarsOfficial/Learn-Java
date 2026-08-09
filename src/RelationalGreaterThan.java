671e11ecadc4c71dd80f4ee8db733406d7b12443
Coding Wars <ind910378369@hotmail.com>
2026-08-09 07:20:13 -0700

    Initial Commit

---
+++ src/RelationalGreaterThan.java
@@ -0,0 +1,20 @@
+/**
+ * Demonstrates the use of Greater Than (>) relational operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class RelationalGreaterThan {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 10;
+
+        // Check whether a is greater than b
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } else {
+            System.out.println(a + " is not greater than " + b);
+        }
+    }
+}
