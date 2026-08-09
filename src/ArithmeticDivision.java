ede741f2762063989b687571f2a37f8ab49c5969
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:48:56 -0700

    Initial Commit

---
+++ src/ArithmeticDivision.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Division (/) arithmetic operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class ArithmeticDivision {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 10;
+
+        // Divide a by b using the division operator
+        int result = a / b;
+
+        // Display the result
+        System.out.println("Division: " + result);
+    }
+}
