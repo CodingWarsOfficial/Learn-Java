b236653b7d74b1d92b997b8608024d859159d419
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:54:34 -0700

    Initial Commit

---
+++ src/UnaryDecrement.java
@@ -0,0 +1,18 @@
+/**
+ * Demonstrates the use of Decrement (--) unary operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class UnaryDecrement {
+    public static void main(String[] args) {
+
+        // Store an initial value
+        int number = 10;
+
+        // Decrease the value by 1
+        number--;
+
+        // Display the updated value
+        System.out.println("Decrement: " + number);
+    }
+}
