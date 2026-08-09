5b12d8af76edae96faf97bf128f3bcc467b916a3
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:37:36 -0700

    Initial Commit

---
+++ src/RelationalLessThanOrEqual.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Less Than or Equal To (<=) relational operator.
+ *
+ * @author Coding Wars
+ */
+public class RelationalLessThanOrEqual {
+    public static void main(String[] args) {
+
+        // Store the speed of a vehicle
+        int speed = 60;
+
+        // Check whether speed is 60 or less
+        if (speed <= 60) {
+            System.out.println("Speed is within the limit.");
+        } else {
+            System.out.println("Speed is above the limit.");
+        }
+    }
+}
