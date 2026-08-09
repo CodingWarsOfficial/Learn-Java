97ebce1a288a742656839fc413d8a5d8a087de64
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:52:58 -0700

    Initial Commit

---
+++ src/UnaryIncrement.java
@@ -0,0 +1,18 @@
+/**
+ * Demonstrates the use of Increment (++) unary operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class UnaryIncrement {
+    public static void main(String[] args) {
+
+        // Store an initial value
+        int number = 10;
+
+        // Increase the value by 1
+        number++;
+
+        // Display the updated value
+        System.out.println("Increment: " + number);
+    }
+}
