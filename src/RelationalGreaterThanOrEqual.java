92a9d1f67f735f2584cbf308d8e8245c1776a648
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:36:35 -0700

    Initial Commit

---
+++ src/RelationalGreaterThanOrEqual.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Greater Than or Equal To (>=) relational operator.
+ *
+ * @author Coding Wars
+ */
+public class RelationalGreaterThanOrEqual {
+    public static void main(String[] args) {
+
+        // Store the person's age
+        int age = 18;
+
+        // Check whether age is 18 or greater
+        if (age >= 18) {
+            System.out.println("You are eligible.");
+        } else {
+            System.out.println("You are not eligible.");
+        }
+    }
+}
