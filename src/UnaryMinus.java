c65fafc66fb195a660e29e35320e79017fa34071
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:52:13 -0700

    Initial Commit

---
+++ src/UnaryMinus.java
@@ -0,0 +1,18 @@
+/**
+ * Demonstrates the use of Unary Minus (-) operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class UnaryMinus {
+    public static void main(String[] args) {
+
+        // Store a positive number
+        int number = 10;
+
+        // Apply the unary minus operator
+        int result = -number;
+
+        // Display the result
+        System.out.println("Unary Minus: " + result);
+    }
+}
