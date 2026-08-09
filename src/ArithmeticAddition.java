771c2825bc19fdcb5e071e1a8ed9e03467145d11
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:46:28 -0700

    Initial Commit

---
+++ src/ArithmeticAddition.java
@@ -0,0 +1,19 @@
+/**
+ * Demonstrates the use of Addition (+) arithmetic operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class ArithmeticAddition {
+    public static void main(String[] args) {
+
+        // Store two numbers
+        int a = 20;
+        int b = 10;
+
+        // Add both numbers using the addition operator
+        int result = a + b;
+
+        // Display the result
+        System.out.println("Addition: " + result);
+    }
+}
