d0271cde38f84095658b5d0a169749e887663712
Coding Wars <ind910378369@hotmail.com>
2026-08-09 09:55:35 -0700

    Initial Commit

---
+++ src/UnaryLogicalNot.java
@@ -0,0 +1,18 @@
+/**
+ * Demonstrates the use of Logical NOT (!) unary operator in Java.
+ *
+ * @author Coding Wars
+ */
+public class UnaryLogicalNot {
+    public static void main(String[] args) {
+
+        // Store a Boolean value
+        boolean isJavaEasy = true;
+
+        // Reverse the Boolean value using the logical NOT operator
+        boolean result = !isJavaEasy;
+
+        // Display the result
+        System.out.println("Logical NOT: " + result);
+    }
+}
